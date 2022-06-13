5ad31de4d885664d51d0f0c8554c7795ef46f72d
f1lrotto <filip.hupka@gmail.com>
2022-06-13 19:05:18 +0200

    swapped with planets controller

--- src/controllers/launches.controller.js
+++ src/controllers/launches.controller.js
@@ -1,7 +1,53 @@
-const { getAllPlanets } = require("../../models/planets.model");
+const {
+  getAllLaunches,
+  scheduleNewLaunch,
+  existsLaunchId,
+  abortLaunchById,
+} = require("./../models/launches.model");
 
-async function httpGetAllPlanets(req, res) {
-  return res.status(200).json(await getAllPlanets());
+const { getPagination } = require("./../services/pagination");
+
+async function httpGetAllLaunches(req, res) {
+  const { skip, limit } = getPagination(req.query);
+  return res.status(200).json(await getAllLaunches(skip, limit));
 }
 
-module.exports = { httpGetAllPlanets };
+async function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "missing launch property",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (launch.launchDate.toString() === "Invalid Date") {
+    return res.status(400).json({
+      error: "invalid date",
+    });
+  }
+
+  await scheduleNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
+async function httpAbortLaunch(req, res) {
+  const id = +req.params.id;
+
+  const exists = await existsLaunchId(id);
+  if (!exists) {
+    return res.status(404).json({
+      error: "launch doesn't exist",
+    });
+  }
+  const aborted = await abortLaunchById(id);
+  return res.status(200).json(aborted);
+}
+
+module.exports = { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch };
